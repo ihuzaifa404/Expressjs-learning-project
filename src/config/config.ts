389e1366e1c2479df4b2e150b655b7640de9b34a
M Huzaifa Butt <zafibutt459@gmail.com>
2026-04-09 19:51:08 +0500

    config

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+//to make object private use _ not actually private
+const _config={
+
+    port:process.env.PORT
+};
+
+//Object.freeze :> to make the object readonly 
+export const config=Object.freeze(_config)
