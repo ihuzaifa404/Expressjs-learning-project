b2391fe53295c5d3640cbe8a494aae60d83f3432
M Huzaifa Butt <zafibutt459@gmail.com>
2026-04-09 19:44:04 +0500

    setup express server

--- server.ts
+++ server.ts
@@ -1,5 +1,10 @@
 import app from "./src/app.js";
 
-app.listen(300, ()=>{
-    console.log("Server is running")
-})
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
+};
+startServer();
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-import express from 'express'
+import express from "express";
 
-const app=express();
+const app = express();
 
+//Routes
 
-
-
+app.get("/", (req, res, next) => {
+  res.json({ message: "Hello this is Product 1" });
+});
 
 export default app;
