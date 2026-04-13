3c2767dd4d993a5785de0fcbd07e308e6852afe1
M Huzaifa Butt <zafibutt459@gmail.com>
2026-04-13 19:36:58 +0500

    add env

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
-import app from "./src/app.js";
-
+import app from './src/app.js';
+import { config } from './src/config/config.js';
 const startServer = () => {
-  const port = process.env.PORT || 3000;
+  const port = config.port || 3000;
 
   app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,6 @@
+import { config as conf } from "dotenv";
+
+conf();
 //to make object private use _ not actually private
 const _config = {
   port: process.env.PORT,
