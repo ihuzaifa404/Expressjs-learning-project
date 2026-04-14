e09f85ffafeb2a6c733b32668af3e6456a7019fd
M Huzaifa Butt <muhammad.huzaifa@cyngro.com>
2026-04-14 20:39:42 +0500

    feat: createUser router

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
-import express, { NextFunction, Request, Response } from 'express';
-import createHttpError, { HttpError } from 'http-errors';
-import { config } from './config/config';
+import express,  from 'express';
 import globalErrorHandler from './middlewares/globalErrorHandler';
+import userRouter from './user/userRouter';
 
 const app = express();
 
 //Routes
 
-app.get('/', (req, res, next) => {
+// app.get('/', (req, res, next) => {
   
   
-   res.json({ message: 'Hello this is Product 1' });
+//    res.json({ message: 'Hello this is Product 1' });
   
-});
+// });
+
+app.use("/api/users",userRouter)
 
 app.use(globalErrorHandler)
 
