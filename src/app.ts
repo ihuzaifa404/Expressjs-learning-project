72dc14f0fbc20ec964d338a2db690f5616839f2c
M Huzaifa Butt <muhammad.huzaifa@cyngro.com>
2026-04-14 20:04:32 +0500

    add globaleErrorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,19 @@
 import express, { NextFunction, Request, Response } from 'express';
 import createHttpError, { HttpError } from 'http-errors';
 import { config } from './config/config';
+import globalErrorHandler from './middlewares/globalErrorHandler';
 
 const app = express();
 
 //Routes
 
 app.get('/', (req, res, next) => {
-  try {
-    const isError = true;
-    if (isError) {
-      const error = createHttpError(400, 'Something went wrong');
-      return next(error);
-    }
-    res.json({ message: 'Hello this is Product 1' });
-  } catch (error) {
-    next(error);
-  }
+  
+  
+   res.json({ message: 'Hello this is Product 1' });
+  
 });
 
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-  const statusCode = err.statusCode || 500;
-
-  return res.status(statusCode).json({
-    message: err.message,
-    errorStack: config.env === 'development' ? err.stack : undefined,
-  });
-});
+app.use(globalErrorHandler)
 
 export default app;
