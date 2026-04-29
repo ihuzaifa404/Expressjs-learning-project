8f2f9ffa7bdd1a12837c5b87c387714e72803e53
M Huzaifa Butt <zafibutt459@gmail.com>
2026-04-29 20:42:53 +0500

    feat: login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -18,51 +18,86 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
   //Database call
 
- try {
-   const user = await userModel.findOne({ email });
+  try {
+    const user = await userModel.findOne({ email });
 
     if (user) {
-    const error = createHttpError(400, 'User already exists');
-    return next(error);
+      const error = createHttpError(400, 'User already exists');
+      return next(error);
+    }
+  } catch (error) {
+    return next(createHttpError(500, 'Error while creating user.'));
   }
 
- } catch (error) {
-  return next(createHttpError(500,"Error while creating user."))
- }
-
- 
-  //hash pasword
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // console.log(hashPassword)
 
-  let newUser:User
+  let newUser: User;
 
- try {
+  try {
     newUser = await userModel.create({
-     name,
-     email,
-     password: hashedPassword,
-   });
- } catch (error) {
-  return next(createHttpError(500,"Error while creating user."))
- }
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return next(createHttpError(500, 'Error while creating user.'));
+  }
 
   //token generation
 
- try {
-   const token = jwt.sign({ sub: newUser._id }, config.jwt as string, {
-     expiresIn: '7d',
-   });
- 
-   //response
-   res.status(201).json({ accessToken: token });
- } catch (error) {
-    return next(createHttpError(500,"Error while signing jwt token!"))
- }
+  try {
+    const token = jwt.sign({ sub: newUser._id }, config.jwt as string, {
+      expiresIn: '7d',
+    });
+
+    res.status(201).json({ accessToken: token });
+
+  } catch (error) {
+    return next(createHttpError(500, 'Error while signing jwt token!'));
+  }
 };
 
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  let { email, password } = req.body;
+
+  if (!email || !password) {
+    const error = createHttpError(400, 'All fields are required!');
+    return next(error);
+  }
+
+let user;
+
+  try {
+     user = await userModel.findOne({ email });
+
+    if (!user) {
+
+      return next(createHttpError(404, 'User not found'));
+    }
+  } catch (error) {
+
+    return next(createHttpError(500, 'Error while Login'));
+  }
+
+  const isMatched = await bcrypt.compare(password, user.password)
+
+  if(!isMatched){
+    return next(createHttpError(401,"Error! email or password is incorrect"))
+  }
 
 
-export { createUser  };
+try {
+  const token = jwt.sign({ sub: user._id }, config.jwt as string, {
+      expiresIn: '7d',
+    });
+    res.status(200).json({ accessToken:token });
+    
+} catch (error) {
+  
+    return next(createHttpError(500, 'Error while signing jwt token!'));
+}
+};
+
+export { createUser, loginUser };
