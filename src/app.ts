de9fbbd69547f3a869b4198f40f00ae877b4b03b
M Huzaifa Butt <zafibutt459@gmail.com>
2026-04-29 23:00:09 +0500

    feat : add multer and use

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,8 @@ app.use(express.json())
 app.use("/api/users",userRouter)
 app.use("/api/books",bookRouter)
 
+
+
 app.use(globalErrorHandler)
 
 export default app;
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -15,7 +15,7 @@ const upload = multer({
 });
 
 bookRouter.post(
-  '/',
+  '/create',
   upload.fields([
     { name: 'coverImage', maxCount: 1 },
     { name: 'file', maxCount: 1 },
