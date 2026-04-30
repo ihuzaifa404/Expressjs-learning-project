1fab2a20fef9f4274259d021b90d1ad500c01073
M Huzaifa Butt <zafibutt459@gmail.com>
2026-04-30 20:48:59 +0500

    feat:  cloudinary logics and implement and upoad coverImage

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,11 +1,27 @@
 import { NextFunction, Request, Response } from 'express';
+import cloudinary from '../config/cloudinary';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  console.log('files:', req.files);
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+  console.log('files:', files);
 
+  const coverImageMimeType = files.coverImage[0].mimetype.split('/').at(-1);
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fileName = files.coverImage[0].filename;
+  const filePath = path.resolve(__dirname, '../../public/data/uploads', fileName);
+  const uploadResult = await cloudinary.uploader.upload(filePath, {
+    filename_override: fileName,
+    folder: 'book-cover',
+    format: coverImageMimeType,
+  });
+
+  console.log('UploadResult:', uploadResult);
   res.status(201).json({ message: 'Book created!' });
 };
-
-
 
 export { createBook };
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,12 +1,15 @@
-import { config as conf } from "dotenv";
+import { config as conf } from 'dotenv';
 
 conf();
 //to make object private use _ not actually private
 const _config = {
   port: process.env.PORT,
-  dbUrl:process.env.MONGOOSE_CONNECTION_STRING,
-  env:process.env.NODE_ENV,
-  jwt:process.env.JWT_SECRET
+  dbUrl: process.env.MONGOOSE_CONNECTION_STRING,
+  env: process.env.NODE_ENV,
+  jwt: process.env.JWT_SECRET,
+  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
 };
 
 //Object.freeze :> to make the object readonly
