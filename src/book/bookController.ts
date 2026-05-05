37d5d69afa3525d60a52bed71d45d0d90428a2d2
M Huzaifa Butt <zafibutt459@gmail.com>
2026-05-05 20:52:15 +0500

    feat: update route and book updated

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -3,75 +3,141 @@ import cloudinary from '../config/cloudinary';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import createHttpError from 'http-errors';
-import fs from "node:fs"
+import fs from 'node:fs';
 import bookModel from './bookModel';
-import  { AuthRequest } from '../middlewares/authenticate'
+import { AuthRequest } from '../middlewares/authenticate';
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const {title, genre}=req.body
+    const { title, genre } = req.body;
     const files = req.files as { [fieldname: string]: Express.Multer.File[] };
 
-
-    if (!files.coverImage || !files.file ||!title||!genre) {
-        return next(createHttpError(400, "All fields are required!."));
+    if (!files.coverImage || !files.file || !title || !genre) {
+      return next(createHttpError(400, 'All fields are required!.'));
     }
     console.log('files:', files);
-  
+
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
     const coverImageMimeType = files.coverImage[0].mimetype.split('/').at(-1);
-  
+
     const fileName = files.coverImage[0].filename;
     const filePath = path.resolve(__dirname, '../../public/data/uploads', fileName);
-  
+
     const uploadImageResult = await cloudinary.uploader.upload(filePath, {
       filename_override: fileName,
       folder: 'book-cover',
       format: coverImageMimeType,
     });
-  
-    
+
     const BookPdfFileName = files.file[0].filename;
     const filePdfPath = path.resolve(__dirname, '../../public/data/uploads', BookPdfFileName);
-    
+
     const uploadFileResult = await cloudinary.uploader.upload(filePdfPath, {
       resource_type: 'auto',
       filename_override: BookPdfFileName,
       folder: 'book-pdfs',
-      
     });
-    
+
     console.log('UploadImageResult:', uploadImageResult);
-    console.log("UploadFileResult: ",uploadFileResult)
+    console.log('UploadFileResult: ', uploadFileResult);
 
-    // @ts-ignore
-    console.log("UserID: ",req.userId )
-    
-    const _req=req as AuthRequest
+    const _req = req as AuthRequest;
 
+    console.log('UserID: ', _req.userId);
 
-    const newBook=await bookModel.create({
+    const newBook = await bookModel.create({
       title,
-      author:_req.userId,
+      author: _req.userId,
       genre,
-      coverImage:uploadImageResult.secure_url,
-      file:uploadFileResult.secure_url
-    })
-    
-    
-        try {
-          await fs.promises.unlink(filePath)
-          await fs.promises.unlink(filePdfPath)
-          
-        } catch (error) {
-             return next(createHttpError(500, "Failed to delete Local files"));
-        }
-    
-    res.status(201).json({ id:newBook._id});
+      coverImage: uploadImageResult.secure_url,
+      file: uploadFileResult.secure_url,
+    });
+
+    try {
+      await fs.promises.unlink(filePath);
+      await fs.promises.unlink(filePdfPath);
+    } catch (error) {
+      return next(createHttpError(500, 'Failed to delete Local files'));
+    }
+
+    res.status(201).json({ id: newBook._id });
   } catch (error) {
-    return next(createHttpError(500, "Error while uploading files"));
+    return next(createHttpError(500, 'Error while uploading files'));
   }
 };
 
-export { createBook };
+const updateBook = async (req: Request, res: Response, next: NextFunction) => {
+  const { title, genre } = req.body;
+
+  const bookId = req.params.bookId;
+
+  const book = await bookModel.findOne({ _id: bookId });
+
+  if (!book) {
+    return next(createHttpError(404, 'Book Not Found!'));
+  }
+
+  const _req = req as AuthRequest;
+  if (book.author.toString() !== _req.userId) {
+    return next(createHttpError(403, "Unauthorized! You can't update other books"));
+  }
+
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+
+  let completeCoverImage = '';
+  if (files.coverImage) {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const coverImageMimeType = files.coverImage[0].mimetype.split('/').at(-1);
+
+    const fileName = files.coverImage[0].filename;
+    const filePath = path.resolve(__dirname, '../../public/data/uploads', fileName);
+
+    completeCoverImage = fileName;
+
+    const uploadImageResult = await cloudinary.uploader.upload(filePath, {
+      filename_override: completeCoverImage,
+      folder: 'book-cover',
+      format: coverImageMimeType,
+    });
+
+    completeCoverImage = uploadImageResult.secure_url;
+    await fs.promises.unlink(filePath);
+  }
+  let completeFile = '';
+  if (files.file) {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const BookPdfFileName = files.file[0].filename;
+    const filePdfPath = path.resolve(__dirname, '../../public/data/uploads', BookPdfFileName);
+
+    completeFile = BookPdfFileName;
+
+    const uploadFileResult = await cloudinary.uploader.upload(filePdfPath, {
+      resource_type: 'auto',
+      filename_override: completeFile,
+      folder: 'book-pdfs',
+    });
+
+    completeFile = uploadFileResult.secure_url;
+    await fs.promises.unlink(filePdfPath);
+  }
+
+  const updatedBook = await bookModel.findOneAndUpdate(
+    {
+      _id: bookId,
+    },
+    {
+      title: title,
+      genre: genre,
+      coverImage: completeCoverImage ? completeCoverImage : book.coverImage,
+      file: completeFile ? completeFile : book.file,
+    },{new: true}
+  );
+
+  res.json(updatedBook);
+};
+
+export { createBook, updateBook };
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createBook } from './bookController';
+import { createBook, updateBook } from './bookController';
 import multer from 'multer';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -23,6 +23,16 @@ bookRouter.post(
     { name: 'file', maxCount: 1 },
   ]),
   createBook,
+);
+
+bookRouter.patch(
+  '/update:bookId',
+  authenticate,
+  upload.fields([
+    { name: 'coverImage', maxCount: 1 },
+    { name: 'file', maxCount: 1 },
+  ]),
+  updateBook,
 );
 
 
